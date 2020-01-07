9ce7e3c5d32bb16860fbef458e3e3ab125cab6ac
Andrew Pond <apond308@gmail.com>
2020-01-07 12:21:41 -0700

    initial commit

---
+++ ImageEditor/src/editor/ImageEditor.java
@@ -0,0 +1,17 @@
+package editor;
+
+public class ImageEditor {
+
+    public static void main(String args[]){
+
+        System.out.println("arg 0");
+        System.out.println(args[0]);
+        System.out.println("arg 1");
+        System.out.println(args[1]);
+
+
+
+
+    }
+
+}
